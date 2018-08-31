85df6bc773092c46edc279abfa5e1cb93a47b2ad
ELehotzky <Lehotzky.Erica@gmail.com>
2018-08-30 22:20:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+	return drivers.filter(function(driver) {
+		if (driver.toLowerCase() === string.toLowerCase()) {
+			return driver;
+		}
+	});
+}
+
+function fuzzyMatch(drivers, string) {
+	return drivers.filter(function(el) {
+		if (el.toLowerCase().startsWith(string.toLowerCase())) {
+			return drivers;
+		}
+	});
+}
+
+function matchName(drivers, string) {
+	return drivers.filter(function(driver) {
+		if (driver.name === string) {
+			return driver;
+		}
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
